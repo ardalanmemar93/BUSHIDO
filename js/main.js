1b4e26121f299fc5eb6a58dc67b4941007927f37
ardalanmemar93 <ardalanmemar@gmail.com>
2023-09-22 08:06:43 -0700

    set up a very basic pseudocode

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-console.log('bushido')
+
+
+// Define an array of directional arrow symbols
+
+
+// Create empty arrays to store user input and the game pattern
+
+
+// Initialize the level and game state variables
+
+
+// Listen for a keypress event to start the game
+
+
+// Listen for button clicks
+ 
+
+// Function to check the user's answer
+
+
+// Function to generate the next sequence
+
+
+// Function to reset the game
+
+
+// Function to animate arrow press
+
+
+// Initialize the game
+
+
+// Event listeners
+
